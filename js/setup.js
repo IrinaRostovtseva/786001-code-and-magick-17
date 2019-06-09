4e9fe49b11c0c37a696eacc08b72749bffdbd8aa
IrinaRostovtseva <ni8003@yandex.ru>
2019-06-09 16:10:12 +0300

    Убирает лишние переменные, добавляет функцию создания рандомного мага и добавлztn магов в setup-similar-list

--- js/setup.js
+++ js/setup.js
@@ -4,9 +4,7 @@ var setup = document.querySelector('.setup');
 var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var similarWizardBlock = document.querySelector('.setup-similar');
 var similarWizardList = document.querySelector('.setup-similar-list');
-var wizardName = wizardTemplate.querySelector('.setup-similar-label');
-var wizardCoat = wizardTemplate.querySelector('.wizard-coat');
-var wizardEyes = wizardTemplate.querySelector('.wizard-eyes');
+var fragment = document.createDocumentFragment();
 
 var USER_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var USER_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -18,17 +16,33 @@ var getRandomArrayElement = function (arr) {
   var randomElemIndex = Math.floor(Math.random() * arr.length);
   return arr[randomElemIndex];
 };
-var getWizardsList = function (arr) {
+var getWizardsList = function () {
   for (var i = 0; i < 4; i++) {
     var player = {
       name: getRandomArrayElement(USER_NAMES) + ' ' + getRandomArrayElement(USER_LAST_NAMES),
       coatColor: getRandomArrayElement(COAT_COLORS),
       eyesColor: getRandomArrayElement(EYES_COLORS)
-    }
-    arr[i] = player;
+    };
+    similarWizards[i] = player;
   }
-  return arr;
+  return similarWizards;
+};
+
+getWizardsList();
+
+var createWizard = function (wizard) {
+  var wizardItem = wizardTemplate.cloneNode(true);
+  wizardItem.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardItem.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardItem.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardItem;
+};
+
+for (var i = 0; i < similarWizards.length; i++) {
+  var wizard = createWizard(similarWizards[i]);
+  fragment.appendChild(wizard);
 }
-console.log(getWizardsList(similarWizards));
+
+similarWizardList.appendChild(fragment);
 setup.classList.remove('hidden');
 similarWizardBlock.classList.remove('hidden');
