bce3781b4966843e277a8815371985768966dd9e
IrinaRostovtseva <ni8003@yandex.ru>
2019-06-16 18:30:02 +0300

    Добавляет пременные для параметров мага, массив с цветами файербола, функцию для изменения цвета и обработчики событий изменения цвета при клике на соответствующем элементе

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,13 @@
 var setup = document.querySelector('.setup');
 var setupOpenButton = document.querySelector('.setup-open');
 var setupCloseButton = setup.querySelector('.setup-close');
-var setupUserName = document.querySelector('.setup-user-name');
+var setupUserName = setup.querySelector('.setup-user-name');
+var setupWizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
+var wizardCoatInput = document.querySelector('.setup-player input[name="coat-color"]');
+var setupWizardEye = setup.querySelector('.setup-wizard .wizard-eyes');
+var wizardEyesInput = document.querySelector('.setup-player input[name="eyes-color"]');
+var setupFireball = setup.querySelector('.setup-fireball-wrap');
+var fireballInput = setupFireball.querySelector('input[name="fireball-color"]');
 var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var similarWizardBlock = document.querySelector('.setup-similar');
 var similarWizardList = document.querySelector('.setup-similar-list');
@@ -14,6 +20,7 @@ var USER_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кр
 var USER_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var similarWizards = [];
 
 var getRandomArrayElement = function (arr) {
@@ -79,3 +86,29 @@ setupCloseButton.addEventListener('keydown', function (evt) {
     setupClose();
   }
 });
+
+// Настройки мага
+var index = 1;
+
+var changeElementColor = function (element, arr, cssProp, inputValue) {
+  element.style = cssProp + ': ' + arr[index] + ';';
+  inputValue.value = arr[index];
+  if (index === arr.length) {
+    index = 0;
+  }
+};
+
+setupWizardCoat.addEventListener('click', function () {
+  changeElementColor(setupWizardCoat, COAT_COLORS, 'fill', wizardCoatInput);
+  index++;
+}, true);
+
+setupWizardEye.addEventListener('click', function () {
+  changeElementColor(setupWizardEye, EYES_COLORS, 'fill', wizardEyesInput);
+  index++;
+}, true);
+
+setupFireball.addEventListener('click', function () {
+  changeElementColor(setupFireball, FIREBALL_COLORS, 'background', fireballInput);
+  index++;
+}, true);
